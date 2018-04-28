16de31791579a570c5622f6da5c7fd7c657edef8
VeraLau1994 <34773236+VeraLau1994@users.noreply.github.com>
2018-04-28 15:18:17 -0400

    Add files via upload

---
+++ install_caffe2/step1_install_dependencies.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install -y wget python-pip python-dev unzip
+sudo pip install six numpy wheel
---
+++ install_caffe2/step2_install_cuda.sh
@@ -0,0 +1,10 @@
+# Install CUDA
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# Add environmental variables
+echo >> .bashrc
+echo "export CUDA_HOME=/usr/local/cuda" >> .bashrc
+echo "export CUDA_ROOT=/usr/local/cuda" >> .bashrc
+echo "export PATH=$PATH:/usr/local/cuda/bin:$HOME/bin" >> .bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64" >> .bashrc
